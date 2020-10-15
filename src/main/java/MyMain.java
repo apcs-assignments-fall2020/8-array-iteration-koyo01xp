470ad68267365c3761019bc8834df9d3ea7a4e22
koyo01xp <21kasakawa@athenian.org>
2020-10-15 07:28:00 +0000

    Resubmission

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -14,43 +14,54 @@ public class MyMain {
 
     // Finds the second largest number in an array
     public static int secondLargest(int[] arr) {
-        int max = 0;
+        int max = -100;
+        int[] arr2 = new int[arr.length];
+        for (int a = 0; a < arr.length; a++) {
+            if (arr[a] > max)
+            {
+                max = arr[a];
+            }
+        }
+            for (int b = 0; b < arr.length; b++)
+            {
+                if (arr[b] == max)
+                {
+                arr[b] = -1000;
+                }
+            }
         
-        for (int i = 0;i<arr.length;i++){
-            if (max < arr[i]) {
-                max = arr[i];
-            }
-            else {
-                max = arr[i];
-            }
-        }
-        int secondmax = arr[0];
-        for (int i = 0;i<arr.length;i++){
-            if (arr[i]!= max){
-                int c = Math.max(secondmax,arr[i]);
-                secondmax = c;
-            }
-            else {
-                continue;
+        arr2 = arr; //array without the maximum value here. 
+        int secondmax = -100;
+        for (int c = 0; c < arr2.length; c++)
+        {
+            if (arr2[c] > secondmax)
+            {
+                secondmax = arr2[c];
             }
         }
-        return secondmax;
+    return secondmax;
     }
 
     // Checks to see if an array contains a geometric series
     public static boolean isGeometric(int[] arr) {
-        for (int i = 0;i < arr.length-1;i++){
-            if (arr[i+1] == arr[i]*2) {
-                continue;
+        boolean x = true;
+        for (int i = 0; i < arr.length - 3; i++) {
+            double var = arr[i];
+            double var2 = arr[i+1];
+            double var3 = arr[i+2];
+            double var4 = arr[i+3];
+            if ((var/var2 == var2/var3) && (var/var2 == var3/var4)){
+                x = true;
             }
-            else {
-                return false;
+
+            else 
+            {
+                x =false;
+                break;
             }
         }
-        return true;
+        return x;
     }
-
-
     public static void main(String[] args) {
         int [] a = {1,2,3,4,5};
         System.out.println(Arrays.toString(reverse(a)));
